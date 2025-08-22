1d5d3ac3d6e5d4978afd80365862f53a3873a887
Arjithar8 <arjithar8@gmail.com>
2025-08-23 00:45:04 +0530

    Finally setup for otel traces atleast a basic one is done

--- manage.py
+++ manage.py
@@ -4,15 +4,12 @@
 import os
 import sys
 
-from opentelemetry.instrumentation.django import DjangoInstrumentor
-
 sys.path.append("src")
 
 
 def main() -> None:
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "over_engineered.settings")
-    DjangoInstrumentor().instrument()
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
